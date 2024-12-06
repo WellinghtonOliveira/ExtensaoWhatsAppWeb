50dab5d96e7ab63f313fb48af9ce729be6fa8e08
Wellinghton O. <71161234+kikeeufalo@users.noreply.github.com>
2024-12-06 19:08:58 -0300

    Update content.js

--- content.js
+++ content.js
@@ -1,12 +1,19 @@
-const mensagem = document.querySelector('.texto')
-const form = document.querySelector('form')
-const btnContatos = document.querySelector('.contatos')
-const nomeSelect = document.querySelector('#contatosNome')
+const mensagem = document.querySelector('.texto');
+const form = document.querySelector('form');
+const btnContatos = document.querySelector('.contatos');
+const nomeSelect = document.querySelector('#contatosNome');
+
+let lista = []
+
+function clickBtnContatos() {
+    console.log(lista);
+
+}
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log(message.lNomes)
-    return true
-})
+    lista.push(message.lNomes)
+    return true; // Para manter o canal aberto (não obrigatório nesse caso).
+});
 
 const contatosMensagens = () => {
     const nomes = [];
@@ -19,16 +26,21 @@ const contatosMensagens = () => {
         }
     });
 
-    chrome.runtime.sendMessage({ lNomes: nomes })
+    // Envia a lista de nomes de volta ao script de fundo
+    chrome.runtime.sendMessage({ lNomes: nomes });
 };
 
-
 btnContatos.addEventListener('click', () => {
-    const [tab] = chrome.tabs.query({ active: true, currentWindow: true });
-    
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        func: contatosMensagens
-    })
-})
+    // Consulta as abas ativas corretamente usando uma Promise
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const tab = tabs[0]; // Obtém a aba ativa
 
+        // Injeta o script na aba ativa
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            func: contatosMensagens
+        });
+    });
+
+    clickBtnContatos()
+});
