f87f8fd1b23b64333157825906bc31ffb2038c7b
Wellinghton O. <71161234+kikeeufalo@users.noreply.github.com>
2024-12-05 22:32:06 -0300

    Update content.js

--- content.js
+++ content.js
@@ -1,1 +1,36 @@
-console.log('Olá Mundo')
+const mensagem = document.querySelector('.texto')
+const form = document.querySelector('form')
+const btnContatos = document.querySelector('.contatos')
+const nomeSelect = document.querySelector('#contatosNome')
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log(message.lNomes)
+    return true
+})
+
+const contatosMensagens = () => {
+    const nomes = [];
+    const listaConversa = document.querySelectorAll('._ak8q');
+
+    listaConversa.forEach((lista) => {
+        const nomeContatos = lista.querySelector('span');
+        if (nomeContatos) {
+            nomes.push(nomeContatos.textContent);
+        }
+    });
+
+    chrome.runtime.sendMessage({ lNomes: nomes })
+};
+
+
+btnContatos.addEventListener('click', () => {
+    const [tab] = chrome.tabs.query({ active: true, currentWindow: true });
+    
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: contatosMensagens
+    })
+
+    console.log('teste')
+})
+
